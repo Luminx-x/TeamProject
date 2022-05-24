5165ebff51b641a3b401ed109a249ef57040185f
zobuo <102634998+zobuo@users.noreply.github.com>
2022-05-23 20:41:09 -0700

    project

--- project/js/mainPage.js
+++ project/js/mainPage.js
@@ -1,60 +1,49 @@
-const canvas = document.getElementById('drawing-board');
-const toolbar = document.getElementById('toolbar');
-const ctx = canvas.getContext('2d');
+$(function() {
+    var letsdraw = false;
 
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
+    var theCanvas = document.getElementById('drawing-board');
+    var toolbar = document.getElementById('toolbar');
+    var ctx = theCanvas.getContext('2d');
+    theCanvas.width = 1200;
+    theCanvas.height = 646;
 
-canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
+    var canvasOffset = $('#drawing-board').offset();
 
-let isPainting = false;
-let lineWidth = 5;
-let startX;
-let startY;
+    toolbar.addEventListener('change', e => {
+        if(e.target.id === 'stroke') {
+            ctx.strokeStyle = e.target.value;
+        }
+    });
+    toolbar.addEventListener('change', e => {
+        if(e.target.id === 'lineWidth') {
+            lineWidth = e.target.value;
+        }
+    })
+    toolbar.addEventListener('click', e => {
+        if (e.target.id === 'clear') {
+            ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+        }
+    });
 
-toolbar.addEventListener('click', e => {
-    if (e.target.id === 'clear') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+    $('#drawing-board').mousemove(function(e) {
+        if (letsdraw === true) {
+            ctx.lineTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
+            ctx.stroke();
+        }
+    });
+
+    $('#drawing-board').mousedown(function() {
+        letsdraw = true;
+        ctx.lineWidth = lineWidth;
+        ctx.lineCap = 'round';
+        ctx.beginPath();
+        ctx.moveTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
+    });
+
+    $(window).mouseup(function() {
+        letsdraw = false;
+    });
 });
-
-toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
-    }
-
-    if(e.target.id === 'lineWidth') {
-        lineWidth = e.target.value;
-    }
-
-});
-
-const draw = (e) => {
-    if(!isPainting) {
-        return;
-    }
-
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = 'round';
-
-    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
-    ctx.stroke();
-}
-
-canvas.addEventListener('mousedown', (e) => {
-    isPainting = true;
-    startX = e.clientX;
-    startY = e.clientY;
-});
-
-canvas.addEventListener('mouseup', e => {
-    isPainting = false;
-    ctx.stroke();
-    ctx.beginPath();
-});
-
-canvas.addEventListener('mousemove', draw);
 
 $("#showPopUp").on("click", function () {
     $(".center").show();
@@ -71,3 +60,69 @@ function toggle() {
   blur.classList.toggle('active');
   var popup = document.getElementById('#showPopUp');
 }
+
+
+
+
+// ------------------------------------------------------------
+// Old code
+
+// const canvas = document.getElementById('drawing-board');
+// const toolbar = document.getElementById('toolbar');
+// const ctx = canvas.getContext('2d');
+// canvas.height = 420;
+// canvas.width = 300;
+//
+// const canvasOffsetX = canvas.offsetLeft;
+// const canvasOffsetY = canvas.offsetTop;
+//
+// canvas.width = window.innerWidth - canvasOffsetX;
+// canvas.height = window.innerHeight - canvasOffsetY;
+//
+// let isPainting = false;
+// let lineWidth = 5;
+// let startX;
+// let startY;
+//
+// toolbar.addEventListener('click', e => {
+//     if (e.target.id === 'clear') {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     }
+// });
+//
+// toolbar.addEventListener('change', e => {
+//     if(e.target.id === 'stroke') {
+//         ctx.strokeStyle = e.target.value;
+//     }
+//
+//     if(e.target.id === 'lineWidth') {
+//         lineWidth = e.target.value;
+//     }
+//
+// });
+//
+// const draw = (e) => {
+//     if(!isPainting) {
+//         return;
+//     }
+//
+//     ctx.lineWidth = lineWidth;
+//     ctx.lineCap = 'round';
+//
+//     ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+//     ctx.stroke();
+// }
+//
+// canvas.addEventListener('mousedown', (e) => {
+//     isPainting = true;
+//     startX = e.clientX;
+//     startY = e.clientY;
+// });
+//
+// canvas.addEventListener('mouseup', e => {
+//     isPainting = false;
+//     ctx.stroke();
+//     ctx.beginPath();
+// });
+//
+// canvas.addEventListener('mousemove', draw);
