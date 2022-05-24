e1c8c854c4cbfab13e582b993d3ff209462e636c
zobuo <102634998+zobuo@users.noreply.github.com>
2022-05-23 18:17:23 -0700

    project

--- project/js/mainPage.js
+++ project/js/mainPage.js
@@ -65,3 +65,9 @@ $("#closePopUp").on('click', function () {
     $(".center").hide();
     $("#showPopUp").show();
 })
+
+function toggle() {
+  var blur = document.getElementById('blur');
+  blur.classList.toggle('active');
+  var popup = document.getElementById('#showPopUp');
+}
