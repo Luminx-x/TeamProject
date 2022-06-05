7081f7908c2fcd9ccf7bc7ba37019b1d9495cede
Tracie <traciethornbury@gmail.com>
2022-06-05 16:09:11 -0700

    Eraser Pencil Undo Redo
    Undo is uhhh
    it isnt perfect but after a couple hours it the best I got, will likely go back to work on it

--- project/js/mainPage.js
+++ project/js/mainPage.js
@@ -7,41 +7,133 @@ $(function() {
     theCanvas.width = 1200;
     theCanvas.height = 646;
 
+    //For Redo function
+    var cStep = -1;
+    var cPushArray = new Array();
+
+    //Needed for the eraser tool
+    const context = theCanvas.getContext('2d')
+    //context.globalCompositeOperation = 'destination-out';
+
+    //Setting initial values
+    context.strokeStyle = stroke.value;
+    lineWidth = lineWidth.value;
+
     var canvasOffset = $('#drawing-board').offset();
 
+
+    //Eraser and pencil tools
+    toolbar.addEventListener('click', e => {
+        if(e.target.id === 'eraser') {
+            context.globalCompositeOperation = 'destination-out';
+        }
+    });
+
+    toolbar.addEventListener('click', e => {
+        if(e.target.id === 'pencil') {
+              context.strokeStyle = stroke.value;
+              context.globalCompositeOperation = 'source-over' // new
+          }
+    });
+
+
+
+    //Color Tool
     toolbar.addEventListener('change', e => {
         if(e.target.id === 'stroke') {
             ctx.strokeStyle = e.target.value;
         }
     });
+
+    //Line Width Tool
     toolbar.addEventListener('change', e => {
         if(e.target.id === 'lineWidth') {
             lineWidth = e.target.value;
         }
     })
+
+    //Clear Tools
     toolbar.addEventListener('click', e => {
         if (e.target.id === 'clear') {
+            /*
+            var image = new Image();
+            image.src = "img/myimg.jpg";
+            console.log(image);
+            ctx.drawImage(image, 0, 0, 500, 200);
+            cPush();
+            */
             ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+            cPush();
         }
     });
 
+    //Undo
+    function cPush() {
+      cStep++;
+      console.log(cStep);
+      if (cStep < cPushArray.length) { cPushArray.length = cStep; }
+      cPushArray.push(theCanvas.toDataURL());
+      console.log(cPushArray[cStep]);
+    }
+    cPush();
+
+    toolbar.addEventListener('click', e => {
+        if (e.target.id === 'undo') {
+          if (cStep > 0) {
+              cStep--;
+              console.log(cStep);
+              var canvasPic = new Image();
+              canvasPic.src = cPushArray[cStep];
+              //ctx.drawImage(canvasPic, 0, 0)
+              canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+              ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+          }
+        }
+    });
+
+    //Redo
+    toolbar.addEventListener('click', e => {
+        if (e.target.id === 'redo') {
+          if (cStep < cPushArray.length-1) {
+              cStep++;
+              console.log(cStep);
+              var canvasPic = new Image();
+              canvasPic.src = cPushArray[cStep];
+              //ctx.drawImage(canvasPic, 0, 0)
+              canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+          }
+        }
+    });
+
+
+    //Drawing Function
     $('#drawing-board').mousemove(function(e) {
         if (letsdraw === true) {
+            //Draw line
             ctx.lineTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
             ctx.stroke();
+
         }
     });
 
-    $('#drawing-board').mousedown(function() {
+    $('#drawing-board').mousedown(function(e) {
         letsdraw = true;
         ctx.lineWidth = lineWidth;
         ctx.lineCap = 'round';
         ctx.beginPath();
         ctx.moveTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
+        cPush();
     });
 
-    $(window).mouseup(function() {
+    $('#drawing-board').mouseleave(function() {
+      if(letsdraw) {
         letsdraw = false;
+      }
+    });
+
+
+    $(window).mouseup(function() {
+      letsdraw = false;
     });
 });
 
