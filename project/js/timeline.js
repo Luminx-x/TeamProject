a9d9c25c467a21c6bb1e28eb2b713cdda3e1fa5c
zobuo <102634998+zobuo@users.noreply.github.com>
2022-06-06 14:07:56 -0700

    project

---
+++ project/js/timeline.js
@@ -0,0 +1,134 @@
+var _URL = window.URL || window.webkitURL;
+
+$("#file2").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas2').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file3").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas3').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file4").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas4').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file5").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas5').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file6").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas6').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file7").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas7').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file8").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas8').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file9").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas9').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
+
+var _URL = window.URL || window.webkitURL;
+
+$("#file10").change(function(e) {
+    var image, file;
+    if ((file = this.files[0])) {
+        image = new Image();
+        image.onload = function() {
+				src = this.src;
+        $('#canvas10').html('<img src="'+ src +'"></div>');
+				e.preventDefault();
+			}
+        };
+        image.src = _URL.createObjectURL(file);
+});
